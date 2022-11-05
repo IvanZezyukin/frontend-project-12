c9253e0139dc30615d99a0db90f2368c93c0e5a2
IvanZezyukin <i.zezukin@gmail.com>
2022-11-05 20:14:07 +0300

    rollbar

--- frontend/src/init.js
+++ frontend/src/init.js
@@ -8,8 +8,18 @@ import App from "./App";
 import { I18nextProvider } from 'react-i18next';
 import i18n from 'i18next';
 import resources from './locales/index';
+import { Provider as RollbarProvider, ErrorBoundary } from '@rollbar/react';
 
 const init = () => {
+
+  const rollbarConfig = {
+    accessToken: '1769bdc0a62741e0b9918ab76332bcbe',
+    captureUncaught: true,
+    captureUnhandledRejections: true,
+    payload: {
+      environment: 'production',
+    },
+  };
 
   i18n.init({
     lng: 'ru',
@@ -20,15 +30,19 @@ const init = () => {
   const socket = io.connect();
 
   return (
-    <Provider store={store}>
-      <I18nextProvider i18n={i18n}>
-        <SocketApiContextProvider socket={socket}>
-          <BrowserRouter>
-            <App />
-          </BrowserRouter>
-        </SocketApiContextProvider>
-      </I18nextProvider>
-    </Provider>
+    <RollbarProvider config={rollbarConfig}>
+      <ErrorBoundary>
+        <Provider store={store}>
+          <I18nextProvider i18n={i18n}>
+            <SocketApiContextProvider socket={socket}>
+              <BrowserRouter>
+                <App />
+              </BrowserRouter>
+            </SocketApiContextProvider>
+          </I18nextProvider>
+        </Provider>
+      </ErrorBoundary>
+    </RollbarProvider>
   )
 };
 
